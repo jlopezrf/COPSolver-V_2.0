b5f865bf797f09847c696eeff6d9d53cbf34f6ca
jason <jalr@openmailbox.org>
2017-05-18 12:03:01 -0300

    se eliminan carpetas y archivos que no deberían estar en el repo los .o y los .cc

--- cpls/util/Maybe.cc
+++
@@ -1,8 +0,0 @@
-/*************************************************/
-/* START of Maybe */
-#include <cpls/util/Maybe.h>
-
-::x10aux::RuntimeType cpls::util::Maybe<void>::rtt;
-
-/* END of Maybe */
-/*************************************************/
--- xxx_main_xxx.cc
+++
@@ -1,3 +0,0 @@
-#include <cpls/Main.h>
-#include <x10aux/bootstrap.h>
-extern "C" { int main(int ac, char **av) { return ::x10aux::template_main< ::cpls::Main>(ac,av); } }
